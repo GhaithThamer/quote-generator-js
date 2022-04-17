e5d9e4aef740532f3502ea7d737ad611c79b0f71
GhaithThamer <ghaith.th@gmail.com>
2022-04-18 01:50:29 +0300

    loader

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 const spanQuote = document.getElementById("spanQuote");
 const divAuthor = document.getElementById("divAuthor");
+const divMainContainer = document.getElementById("divMainContainer");
 const buttonNextQuote = document.getElementById("buttonNextQuote");
 const buttonTweet = document.getElementById("buttonTweet");
+const loader = document.getElementById("loader");
 
 const apiAdress = "https://type.fit/api/quotes"
 let jsonQuote;
 extractQuote();
 async function extractQuote(){
+    loading();
     stringsQuote= await fetch(apiAdress);
     jsonQuote= await stringsQuote.json();
     chooseAndDisplayQuote();
 }
 
 function chooseAndDisplayQuote(){
+    loading();
     const randomNumberOfThisTry = pickRandomNumber(0,jsonQuote.length-1)
     spanQuote.innerText=jsonQuote[randomNumberOfThisTry].text;
     divAuthor.innerText=jsonQuote[randomNumberOfThisTry].author;
+    loadingComplete();
     //console.log(jsonQuote)
 }
 buttonNextQuote.addEventListener("click",()=>{
@@ -31,4 +36,14 @@ buttonTweet.addEventListener("click",()=>{
 function pickRandomNumber(lowest,highest){
     const randomNumber = Math.floor(Math.random()*(highest-lowest))+lowest;
 return randomNumber;
+}
+
+function loading(){
+    loader.hidden = false;
+    divMainContainer.hidden = true;
+}
+
+function loadingComplete(){
+    loader.hidden = true;
+    divMainContainer.hidden = false;
 }
